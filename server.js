51fa46bc5c4b028a74307ab043bb698eec9152ea
Kamil <xkamilwrobel@gmail.com>
2018-08-26 01:01:31 +0200

    added example route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,12 +16,22 @@ const port = process.env.PORT || 8080;
 const router = express.Router();
 
 router.get('/test', function (req, res) {
-    res.json({message: 'hooray! welcome to our api!'});
+    res.json({message: 'hooray! welcome to our api!' + new Date().getMilliseconds()});
 });
 
+router.get('/portals/:name', function (req, res) {
+    const path = `./portals/${req.params.name}.json`;
+
+    if (fs.existsSync(path)) {
+        const portal = require(path);
+        res.json(portal);
+    } else {
+        res.status(404).json({message: `portal ${req.params.name} does not exists.`})
+    }
+});
 
 
 app.use('/api', router);
 
-app.listen(port, ()=>console.log('Server started on port ' + port));
+app.listen(port, () => console.log('Server started on port ' + port));
 
